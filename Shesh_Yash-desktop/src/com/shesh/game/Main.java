684275346bed20938691c67756fdae828c4ca60d
yashshah12 <ybshah12@gmail.com>
2014-12-23 13:12:05 -0800

    First commit

---
+++ Shesh_Yash-desktop/src/com/shesh/game/Main.java
@@ -0,0 +1,17 @@
+package com.shesh.game;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Space Run!";
+		cfg.useGL20 = true;
+		cfg.width = 800;
+		cfg.height = 480;
+		cfg.resizable = true;
+		
+		new LwjglApplication(new SideScroller(), cfg);
+	}
+}
